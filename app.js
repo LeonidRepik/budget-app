d89c7251fc72fced354f468fb39d3470d309698c
Leonid Repik <leonidrepik@MacBook-Pro-Leonid.local>
2019-09-18 13:28:33 -0700

    New Data storage

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 //BUDGET CONTROLLER
 var budgetController =(function(){
     
+    var Expense = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+    var data = {
+        allItems: {
+            exp : [],
+            inc :[]
+        },
+        totals: {
+            exp:0,
+            inc:0
+        }
+       
+    };
 })();
 
 
@@ -12,7 +36,8 @@ var UIController = (function(){
     var DOMtrings = {
         inputType:'.add__type',
         inputDesription:'.add__description',
-        inputValue:'.add__value'
+        inputValue:'.add__value',
+        inputBtn:'.add__btn'
     };
     
     return{
@@ -37,19 +62,38 @@ var UIController = (function(){
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-    var DOM =  UICtrl.getDOMstrings;
-    var ctrlAddItem = function(){
+    var setupEventListners = function(){
+        var DOM =  UICtrl.getDOMstrings();
 
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+        document.addEventListener('keypress',function(event){
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
+
+
+    var ctrlAddItem = function(){
+        // 1. Get the field input data
         var input = UIController.getinput();
-        console.log(input);
-    }
+    };
     
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
-    
-    document.addEventListener('keypress',function(event){
-        if(event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    // 2. Add the item to the budget
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
+    return{
+        init:function(){
+            console.log('App started');
+            setupEventListners();
         }
-    });
+    }
+
 })(budgetController,UIController);
  
+controller.init(); 
