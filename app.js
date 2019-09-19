896ad196961e770fc1f0cd29a9e6337620f2ceaf
Leonid Repik <leonidrepik@MacBook-Pro-Leonid.local>
2019-09-18 19:45:16 -0700

    Set data for total income, ctrated calc income function

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ var budgetController =(function(){
         this.value = value;
     };
 
+    var calculateTotal = function(type){
+
+        var sum = 0;
+
+        data.allItems[type].forEach(function(val){
+            sum = sum+val.value;
+        });
+
+        data.totals[type] = sum;
+
+    };
 
     var data = {
         allItems: {
@@ -22,8 +33,9 @@ var budgetController =(function(){
         totals: {
             exp:0,
             inc:0
-        }
-       
+        },
+        budget:0,
+        percentage:-1
     };
 
     return{
@@ -52,6 +64,41 @@ var budgetController =(function(){
 
             // return our data
             return newItem;
+        },
+
+        calculateBudget:function(){
+
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+
+            // Calculate the precetage of income that we spent
+            if(data.totals.inc > 0){
+                data.percentage = Math.round((data.totals.exp/data.totals.inc) * 100);
+            } else {
+                data.precentege = -1;
+            }
+
+
+
+        },
+
+        getBudget: function(){
+
+            return {
+
+                budget: data.budget,
+                totalIncome: data.totals.inc,
+                totalExp: data.totals.exp,
+                precentege:data.percentage
+
+            }
+
+
         },
 
         testing: function(){
@@ -162,11 +209,13 @@ var controller = (function(budgetCtrl, UICtrl){
     var updateBudget = function(){
 
     // 1. Calculate the budget
+    budgetController.calculateBudget();
 
     // 2. return the budget
+        var budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
-
+        console.log(budget);
     }
 
     var ctrlAddItem = function(){
