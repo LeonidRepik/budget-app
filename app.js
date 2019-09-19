5cab6a4b563cd762fbbd2de4fc8a031471c9bed6
Leonid Repik <leonidrepik@MacBook-Pro-Leonid.local>
2019-09-18 17:52:49 -0700

    add Data function added

--- app.js
+++ app.js
@@ -25,6 +25,39 @@ var budgetController =(function(){
         }
        
     };
+
+    return{
+        addItem:function(type,des,val){
+            var newItem, ID;
+
+            // Create new ID 
+            if(data.allItems[type].length > 0 ){
+
+                ID = data.allItems[type][data.allItems[type].length - 1 ].id +1;
+
+            } else {
+
+                ID = 0;
+            }
+
+            // Create new item based on inc or exp type
+            if(type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc'){
+                newItem = new Income(ID, des, val);                
+            }
+
+            // Push it into our data structure 
+            data.allItems[type].push(newItem);
+
+            // return our data
+            return newItem;
+        },
+
+        testing: function(){
+            console.log(data);
+        }
+    }
 })();
 
 
@@ -75,17 +108,23 @@ var controller = (function(budgetCtrl, UICtrl){
 
 
     var ctrlAddItem = function(){
-        // 1. Get the field input data
-        var input = UIController.getinput();
-    };
+        var input,newItem;
+
+    // 1. Get the field input data
+
+        input = UIController.getinput();
     
     // 2. Add the item to the budget
+
+        newItem = budgetController.addItem(input.type, input.description,input.value);
+
 
     // 3. Add the item to the UI
 
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
+};
 
     return{
         init:function(){
@@ -97,3 +136,4 @@ var controller = (function(budgetCtrl, UICtrl){
 })(budgetController,UIController);
  
 controller.init(); 
+console.log(data);
